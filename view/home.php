cb3de4b87247de1567694c3ddc98f9edae370f35
RaulBenitezCorrea <rauly2402@gmail.com>
2025-04-09 15:05:45 -0600

    cambios css

--- view/home.php
+++ view/home.php
@@ -4,17 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/assets/css/custom.css"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
 </head>
 <body>
-    <h1>Bienvenido a la página principal</h1>
-    <p>Esta es la vista de inicio.</p>
 
-    <!-- Botones para navegar a otras vistas -->
-    <a href="<?php echo BASE_URL; ?>usuarios/listar">
-        <button>Ir a Usuarios</button>
-    </a>
-    <a href="<?php echo BASE_URL; ?>productos/ver">
-        <button>Ir a Productos</button>
-    </a>
+    <!-- Navbar -->
+    <div class="row">
+        <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><i class="fas fa-store-alt ms-2 me-2"></i> MiTienda</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Mi Tienda</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Registrar Mi Tienda</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </div>
+    <!-- Fin Navbar -->
+
+
+    <script src="https://kit.fontawesome.com/33f535655a.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
