f2a75aae8021c7055b597ec28c8f7bdc377fa786
RaulBenitezCorrea <rauly2402@gmail.com>
2025-04-09 14:42:24 -0600

    aa

--- view/home.php
+++ view/home.php
@@ -6,15 +6,6 @@
     <title>Inicio</title>
 </head>
 <body>
-    <h1>Bienvenido a la página principal</h1>
-    <p>Esta es la vista de inicio.</p>
 
-    <!-- Botones para navegar a otras vistas -->
-    <a href="<?php echo BASE_URL; ?>usuarios/listar">
-        <button>Ir a Usuarios</button>
-    </a>
-    <a href="<?php echo BASE_URL; ?>productos/ver">
-        <button>Ir a Productos</button>
-    </a>
 </body>
 </html>
