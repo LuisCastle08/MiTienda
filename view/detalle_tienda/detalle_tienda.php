08ed887374eaa6c98c35e096773dacb0b97392cb
RaulBenitezCorrea <rauly2402@gmail.com>
2025-04-23 15:55:26 -0600

    cambio de vista en detalle de la tienda

--- view/detalle_tienda/detalle_tienda.php
+++ view/detalle_tienda/detalle_tienda.php
@@ -22,7 +22,49 @@
 
     <!-- Herramientas -->
     <div class="row d-flex justify-content-between align-items-start">
-        <label for="exampleFormControlInput1" class="form-label text-center col-12"><h5>Herramientas</h5></label>
+        <div class="col-6 mt-2">
+            <a href="#" class="text-decoration-none">
+                <div class="card overflow-hidden card_menu h-100">
+                    <div class="card-body d-flex flex-column align-items-center justify-content-center text-center">
+                        <div class="text-center">
+                            <div class="profile-photo">
+                                <img src="https://cdn-icons-png.flaticon.com/512/1330/1330833.png"  width="100" class="img-fluid " alt="">
+                            </div>
+                            <h3 class="mt-4 mb-1 text-wrap">1500 Vistas</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-6 mt-2">
+            <a href="<?php echo BASE_URL; ?>InventarioTienda" class="text-decoration-none">
+                <div class="card overflow-hidden card_menu h-100">
+                    <div class="card-body d-flex flex-column align-items-center justify-content-center text-center">
+                        <div class="text-center">
+                            <div class="profile-photo">
+                                <img src="https://cdn-icons-png.flaticon.com/512/2674/2674486.png"  width="100" class="img-fluid " alt="">
+                            </div>
+                            <h3 class="mt-4 mb-1 text-wrap">Inventario</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-12 mt-3">
+            <a href="#" class="text-decoration-none">
+                <div class="card overflow-hidden card_menu h-100">
+                    <div class="card-body d-flex flex-column align-items-center justify-content-center text-center">
+                        <div class="text-center">
+                            <div class="profile-photo">
+                                <img src="https://cdn-icons-png.flaticon.com/512/9580/9580634.png"  width="100" class="img-fluid " alt="">
+                            </div>
+                            <h3 class="mt-4 mb-1 text-wrap">Soporte a Usuarios</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!--<label for="exampleFormControlInput1" class="form-label text-center col-12"><h5>Herramientas</h5></label>
         <div class="col-12 d-flex justify-content-center">
             <a href="#" class="text-decoration-none text-dark w-100 d-flex justify-content-center">
                 <div class="card col-10 col-lg-5 mb-3" style="background-color: #FFC107">
@@ -33,7 +75,7 @@
             </a>
         </div>
         <div class="col-12 d-flex justify-content-center">
-            <a href="<?php echo BASE_URL; ?>InventarioTienda" class="text-decoration-none text-dark w-100 d-flex justify-content-center">
+            <a href="<?php /*echo BASE_URL;*/ ?>InventarioTienda" class="text-decoration-none text-dark w-100 d-flex justify-content-center">
                 <div class="card col-10 col-lg-5 mb-3" style="background-color: #FFC107">
                     <div class="card-body text-center">
                         <h6><i class="fas fa-box me-2"></i>Inventario</h6>
@@ -58,7 +100,7 @@
                     </div>
                 </div>
             </a>
-        </div>
+        </div>-->
     </div>
     <!-- Fin Herramientas -->
 
