42317deea999ac53a3b8d93df7c023b29cb1dec7
RaulBenitezCorrea <rauly2402@gmail.com>
2025-04-23 15:36:23 -0600

    inclui vista en navbar

--- view/shared/navbar.php
+++ view/shared/navbar.php
@@ -35,6 +35,9 @@
                                 <li class="nav-item">
                                     <a class="nav-link active" aria-current="page" href="'.BASE_URL.'LoginTienda">Login Tienda</a>
                                 </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="'.BASE_URL.'DetalleTienda">Detalle Tienda</a>
+                                </li>
                                 ';
                                 break;
                             default:
